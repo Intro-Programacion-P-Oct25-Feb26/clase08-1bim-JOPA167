e12197bf5651ef7836bbd8e7490331c0fb6d231d
JOPA167 <joparajirj@gmail.com>
2025-11-26 12:00:01 -0500

    solución problema 2.1

---
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo021.java
@@ -0,0 +1,50 @@
+/*
+ Al ejecutar el siguiente código, el usuario puede salir del ciclo cuando
+ingrese las siguientes opciones
+s
+si
+yes
+y
+para la solución usar switch
+ */
+package ejemplos02;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo021 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        String cadenaFinal = "";
+        double nota;
+        boolean bandera = true;
+        String salida;
+        do {
+            System.out.println("Ingrese calificación");
+            nota = entrada.nextDouble();
+            cadenaFinal = String.format("%s%.2f\n", cadenaFinal, nota);
+            entrada.nextLine(); // limpieza de buffer
+
+            System.out.println("Ingrese (s,si,yes,y) si desea salir del ciclo ");
+            salida = entrada.nextLine();
+
+            switch (salida) {
+                case "yes":
+                case "y":
+                case "si":
+                case "s":
+                    bandera = false;
+                    break;
+            }
+
+        } while (bandera); // (bandera==true)
+
+        System.out.printf("Listado de Notas\n%s\n", cadenaFinal);
+    }
+}
